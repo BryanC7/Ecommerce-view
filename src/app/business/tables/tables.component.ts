fdc949fe0dba08bda11128bb4856d8f6b2ef418d
Bryan Cabrera <bryancabreram3@gmail.com>
2024-10-04 22:53:55 -0300

    build: First implementations of the Sweet Alert and Font Awesome libraries

--- src/app/business/tables/tables.component.ts
+++ src/app/business/tables/tables.component.ts
@@ -1,19 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { UsersService } from '../../core/services/users.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TablesComponent {
   users: any = [];
+  faPencil = faPencil;
+  faTrash = faTrash;
 
   constructor(private usersService: UsersService, private cdr: ChangeDetectorRef) {}
 
@@ -27,5 +30,9 @@ export default class TablesComponent {
         console.error('Error fetching users:', err);
       }
     });
+  }
+  
+  update(): void {
+    Swal.fire("SweetAlert2 is working!");
   }
 }
