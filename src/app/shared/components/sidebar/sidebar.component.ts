114a5fdb59ca709602eb3dd9baa990120d77766e
Bryan Cabrera <bryancabreram3@gmail.com>
2024-10-07 20:31:00 -0300

    style: Correcting styles and icons in the table and sidebar component

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -2,17 +2,23 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTable, faHome, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent { 
 isAdmin: boolean = false;
+faTable = faTable;
+faHome = faHome;
+faRightFromBracket = faRightFromBracket;
+faUser = faUser;
 
   constructor(private authService: AuthService) {}
 
